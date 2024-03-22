500225c2b165e599f6974b96c112f362082b00a6
Cecer <cecer_git@cecer1.com>
2024-03-22 14:11:06 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+A quick and dirty bot to squash poll spam until Discord gets their shit together. 
+
+# Requirements
+- Requires node 16 or later. Tested with `16.20.2` and `21.1.0`.
+
+# Basic setup
+- Clone this repo.
+- Run `npm install` in it
+- Edit line 1 of `index.js` so it contains the token of your bot.
+
+# Running the bot
+- Run `node index.js`
+
---
+++ index.js
@@ -0,0 +1,27 @@
+const BOT_TOKEN = "BOT_TOKEN_HERE";
+
+const {Client, GatewayIntentBits, GatewayDispatchEvents, Message} = require("discord.js");
+
+class CustomClient extends Client {
+    
+    constructor() {
+        super({
+            intents: [
+                GatewayIntentBits.Guilds, 
+                GatewayIntentBits.GuildMessages, 
+                GatewayIntentBits.MessageContent
+            ]
+        });
+
+        // Process the raw message creatoin event on the web socket.
+        this.ws.on(GatewayDispatchEvents.MessageCreate, data => {
+            if (data.poll) { // If it has a truthy poll field, it's a poll.
+                console.log(`Deleting poll ${data.id} by ${data.author.id} in ${data.channel_id}`);
+                new Message(this, data).delete();
+            }
+        });
+    }
+}
+
+
+new CustomClient().login(BOT_TOKEN);
